72296b3c7573fc9903efb450e4a5200655e6c40d
Jack Butts <buttsjofficial@gmail.com>
2017-12-16 12:40:07 -0500

    Good to go

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
 # pbi-bubbles
+> A Power BI visualization to create a 'chatroom' with bubble messages :iphone: :speech_balloon:
+
+## How it works
+
+Give the visualization left/right bubble values and they will appear in a stack that is scrollable.
+
+![Image of Visualization1](https://github.com/buttsj/pbi-bubbles/blob/master/assets/example1.png)
+
+![Image of Visualization2](https://github.com/buttsj/pbi-bubbles/blob/master/assets/example2.png)
+
+![Image of Visualization3](https://github.com/buttsj/pbi-bubbles/blob/master/assets/example3.png)
+
+![Image of Decorated Visual 1](https://github.com/buttsj/pbi-bubbles/blob/master/assets/example4.png) ![Image of Decorated Visual 2](https://github.com/buttsj/pbi-bubbles/blob/master/assets/example5.png) ![Image of Decorated Visual 2](https://github.com/buttsj/pbi-bubbles/blob/master/assets/example6.png) ![Image of Decorated Visual 2](https://github.com/buttsj/pbi-bubbles/blob/master/assets/example7.png)
+
+## Installing this Visualization
+#### Option 1 - GitHub
+Navigate to the /dist/ folder and download the .pbiviz file
+
+#### Option 2 - Microsoft AppSource Store
+Coming soon!
+
+#### Installing in Power Bi Desktop/Web App
+
+![Image of Icon](https://github.com/buttsj/pbi-bubbles/blob/master/assets/bubble_icon.png)
+
+In the list of visuals, click the ellipsis :speech_balloon: and import the .pbiviz file into your report.
+
+## Contributors
+
+Jack Butts [[Github]](https://github.com/buttsj)
+
+## Release History
+
+* v1.0
+	* Bubbles are created for each row supplied to the respective data bucket
+    * Bubble's background color is selectable
+    * Bubble's text color is selectable
+    * Set the background color of the visual
+    * Click a bubble to filter other visuals, and same with the other direction
+
+***
+###### Thanks
+    * Code examples & inspiration by http://www.smartjava.org/content/render-open-data-3d-world-globe-threejs
+    * Code examples & inspiration by http://stemkoski.github.io/Three.js/Labeled-Geometry.html
--- src/visual.ts
+++ src/visual.ts
@@ -31,14 +31,8 @@ module powerbi.extensibility.visual {
         private settings: VisualSettings;
         private selectionIdBuilder: ISelectionIdBuilder;
         private selectionManager: ISelectionManager;
-        private selectedBubbleLoc: string;
-        private selectedBubbleSide: number;
-        private leftBubbleList: HTMLElement[];
-        private rightBubbleList: HTMLElement[];
 
         constructor(options: VisualConstructorOptions) {
-            this.selectedBubbleLoc = "";
-            this.selectedBubbleSide = 0;
             this.target = options.element;
             this.host = options.host;
             this.selectionIdBuilder = this.host.createSelectionIdBuilder();
@@ -47,10 +41,10 @@ module powerbi.extensibility.visual {
                 const new_ul: HTMLElement = document.createElement("ul");
                 const new_li1: HTMLElement = document.createElement("li");
                 new_li1.setAttribute("class", "him");
-                new_li1.appendChild(document.createTextNode("Add data to the left side!"));
+                new_li1.appendChild(document.createTextNode("⠀⠀⠀⠀⠀⠀⠀⠀⠀"));
                 const new_li2: HTMLElement = document.createElement("li");
                 new_li2.setAttribute("class", "me");
-                new_li2.appendChild(document.createTextNode("...or add data to the right side!"));
+                new_li2.appendChild(document.createTextNode("⠀⠀⠀⠀⠀⠀⠀⠀⠀"));
                 new_ul.appendChild(new_li1);
                 new_ul.appendChild(new_li2);
                 this.target.appendChild(new_ul);
@@ -59,19 +53,16 @@ module powerbi.extensibility.visual {
 
         public update(options: VisualUpdateOptions) {
             this.settings = Visual.parseSettings(options && options.dataViews && options.dataViews[0]);
-            this.leftBubbleList = [];
-            this.rightBubbleList = [];
-            let leftIDS: any[] = [];
-            let rightIDS: any[] = [];
             if (!options.dataViews) return;
             let dataView: DataView = options && options.dataViews && options.dataViews[0];
             if (dataView.categorical.categories.length > 0) {
+                debugger;
                 while (this.target.firstChild) {
                     this.target.removeChild(this.target.firstChild)
                 }
                 let bubbleIDS = Visual.getSelectionIds(dataView, this.host);
                 let lBubble: any[] = dataView.categorical.categories[0].values as any[];
-                let rBubble: any[];
+                let rBubble: any[] = [];
                 if (dataView.categorical.categories.length > 1) {
                     rBubble = dataView.categorical.categories[1].values as any[];
                 }
@@ -80,81 +71,33 @@ module powerbi.extensibility.visual {
                     if (i < lBubble.length) {
                         const new_li1: HTMLElement = document.createElement("li");
                         new_li1.setAttribute("class", "him");
-                        new_li1.style.opacity = "1";
                         new_li1.setAttribute("id", i.toString());
                         new_li1.setAttribute("style", "background: " + this.settings.dataPoint.leftColor + "; color: " + this.settings.dataPoint.leftFont + ";");
                         new_li1.appendChild(document.createTextNode(lBubble[i]));
                         new_li1.onclick = (ev:MouseEvent) => {
-                            for (let bub of this.leftBubbleList) {
-                                bub.style.opacity = "1";
-                            }
-                            for (let bub of this.rightBubbleList) {
-                                bub.style.opacity = "1";
-                            }
                             this.selectionManager.select(bubbleIDS[new_li1.getAttribute("id")]).then((ids: ISelectionId[]) => {
-                                new_li1.style.opacity = "0.5";
-                                this.selectedBubbleLoc = new_li1.getAttribute("id");
-                                this.selectedBubbleSide = 0;
                             });
                             ev.stopPropagation();
                         };
                         new_ul.appendChild(new_li1);
-                        this.leftBubbleList.push(new_li1);
                     }
                     if (i < rBubble.length) {
                         const new_li2: HTMLElement = document.createElement("li");
                         new_li2.setAttribute("class", "me");
-                        new_li2.style.opacity = "1";
                         new_li2.setAttribute("id", i.toString());
                         new_li2.setAttribute("style", "background: " + this.settings.dataPoint.rightColor + "; color: " + this.settings.dataPoint.rightFont + ";");
                         new_li2.appendChild(document.createTextNode(rBubble[i]));
                         new_li2.onclick = (ev:MouseEvent) => {
-                            for (let bub of this.leftBubbleList) {
-                                bub.style.opacity = "1";
-                            }
-                            for (let bub of this.rightBubbleList) {
-                                bub.style.opacity = "1";
-                            }
                             this.selectionManager.select(bubbleIDS[new_li2.getAttribute("id")]).then((ids: ISelectionId[]) => {
-                                new_li2.style.opacity = "0.5";
-                                this.selectedBubbleLoc = new_li2.getAttribute("id");
-                                this.selectedBubbleSide = 1;
                             });
                             ev.stopPropagation();
                         };
                         new_ul.appendChild(new_li2);
-                        this.rightBubbleList.push(new_li2);
-                    }
-                }
-                if (!this.selectionManager.hasSelection) {
-                    for (let bub of this.leftBubbleList) {
-                        bub.style.opacity = "1";
-                    }
-                    for (let bub of this.rightBubbleList) {
-                        bub.style.opacity = "1";
-                    }
-                    this.selectedBubbleLoc = "";
-                    this.selectedBubbleSide = 0
-                }
-                if (this.selectedBubbleLoc.length > 0) {
-                    if (this.selectedBubbleSide === 0) {
-                        this.leftBubbleList[this.selectedBubbleLoc].style.opacity = "0.5";
-                    } 
-                    else {
-                        this.rightBubbleList[this.selectedBubbleLoc].style.opacity = "0.5";
                     }
                 }
                 this.target.appendChild(new_ul);
                 this.target.onclick = (ev:MouseEvent) => {
                     this.selectionManager.clear().then(() => {
-                        for (let bub of this.leftBubbleList) {
-                            bub.style.opacity = "1";
-                        }
-                        for (let bub of this.rightBubbleList) {
-                            bub.style.opacity = "1";
-                        }
-                        this.selectedBubbleLoc = "";
-                        this.selectedBubbleSide = 0;
                     });
                     ev.stopPropagation();
                 };
@@ -168,7 +111,6 @@ module powerbi.extensibility.visual {
                     values: null,
                     identity: [identity]
                 };
-        
                 return host.createSelectionIdBuilder()
                     .withCategory(categoryColumn, 0)
                     .createSelectionId();
